c18e2ae5c2c3d3ddef16edc6f77c54932ff80c06
Agreatdayforfreedom <matias.bertoni2002@gmail.com>
2024-06-03 15:28:38 -0300

    init

--- src/adapters/db-adapter.module.ts
+++ src/adapters/db-adapter.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { dbAdapter } from './db.adapter';
 import { DB } from '../extern/db';
+import { DBModule } from '../extern/db.module';
 
 @Module({
+  imports: [DBModule],
   providers: [dbAdapter, DB],
   exports: [dbAdapter],
 })
--- src/use-cases/todo/todo.module.ts
+++ src/use-cases/todo/todo.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { UCTodo } from './todo.use-case';
 import { dbAdapter } from '../../adapters/db.adapter';
 import { DB } from '../../extern/db';
+import { DbAdapterModule } from '../../adapters/db-adapter.module';
+import { TodoController } from '../../controllers/todo.controller';
 
 @Module({
-  providers: [UCTodo, dbAdapter, DB],
+  imports: [DbAdapterModule],
+  controllers: [TodoController],
+  providers: [UCTodo],
   exports: [UCTodo],
 })
 export class TodoModule {}
